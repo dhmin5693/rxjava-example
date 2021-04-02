c47936272fb106597a7363618e383cc8a16f2d51
dhmin5693 <dhmin5693@naver.com>
2021-04-02 22:33:45 +0900

    add new example

---
+++ src/main/java/chap3/sub3_1/CustomSubscriber.java
@@ -0,0 +1,33 @@
+package chap3.sub3_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+class CustomSubscriber<T> implements Subscriber<T> {
+
+    private List<T> list = new ArrayList<>();
+
+    @Override
+    public void onNext(T t) {
+        if (list.size() < 5) {
+            list.add(t);
+        }
+    }
+
+    @Override
+    public void onSubscribe(Subscription s) {
+        System.out.println("onSubscribe");
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        t.printStackTrace();
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("onComplete");
+    }
+}
