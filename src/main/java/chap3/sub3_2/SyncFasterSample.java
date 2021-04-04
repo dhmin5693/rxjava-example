d9d7021bceb185ab876a9877ca165b09e96ff7e7
dhmin5693 <dhmin5693@naver.com>
2021-04-04 13:33:11 +0900

    add new example

--- src/main/java/chap3/sub3_2/SyncFasterSample.java
+++ src/main/java/chap3/sub3_2/SyncFasterSample.java
@@ -3,13 +3,12 @@ package chap3.sub3_2;
 import io.reactivex.Flowable;
 import java.util.concurrent.TimeUnit;
 
-public class SyncSlowerSample {
+public class SyncFasterSample {
 
     public static void main(String[] args) throws InterruptedException {
-
         Flowable.interval(1000L, TimeUnit.MILLISECONDS)
                 .doOnNext(System.out::println)
-                .subscribe(data -> Thread.sleep(2000L));
+                .subscribe(data -> Thread.sleep(500L));
 
         Thread.sleep(10000L);
     }
