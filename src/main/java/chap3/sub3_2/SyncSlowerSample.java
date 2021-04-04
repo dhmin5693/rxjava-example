2d2d58402014bf41ce87e343860270d5c96cea64
dhmin5693 <dhmin5693@naver.com>
2021-04-04 13:28:56 +0900

    add new example

--- src/main/java/chap3/sub3_2/SyncSlowerSample.java
+++ src/main/java/chap3/sub3_2/SyncSlowerSample.java
@@ -9,8 +9,8 @@ public class SyncSlowerSample {
 
         Flowable.interval(1000L, TimeUnit.MILLISECONDS)
                 .doOnNext(data -> System.out.println("emit: " + System.currentTimeMillis() + ", 밀리초: " + data))
-                .subscribe(data -> Thread.sleep(500L));
+                .subscribe(data -> Thread.sleep(2000L));
 
-        Thread.sleep(3000L);
+        Thread.sleep(10000L);
     }
 }
