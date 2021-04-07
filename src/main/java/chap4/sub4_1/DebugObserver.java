e7d5a52016433604f82e53da7b8b6b1098cd694b
dhmin5693 <dhmin5693@naver.com>
2021-04-07 17:53:09 +0900

    add DebugObserver

---
+++ src/main/java/chap4/sub4_1/DebugObserver.java
@@ -0,0 +1,52 @@
+package chap4.sub4_1;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DebugObserver<T> implements Observer<T> {
+
+    private String label;
+
+    public DebugObserver() {
+        super();
+    }
+
+    public DebugObserver(String label) {
+        super();
+        this.label = label;
+    }
+
+    private String getMiddleMessage() {
+        return label != null ? label + ": " : "";
+    }
+
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+        log.info("{}{}, {}", getMiddleMessage(), "subscribe", d);
+    }
+
+    @Override
+    public void onNext(@NonNull T data) {
+        log.debug("{}{}", getMiddleMessage(), data);
+    }
+
+    @Override
+    public void onError(@NonNull Throwable e) {
+        log.error("{}{}", getMiddleMessage(), e);
+    }
+
+    @Override
+    public void onComplete() {
+        log.info("{}{}", getMiddleMessage(), "complete");
+    }
+
+    public static void main(String[] args) {
+        Observable.just(1, 2, 3, 4, 5)
+                  .subscribe(new DebugObserver<>("tester"));
+    }
+}
